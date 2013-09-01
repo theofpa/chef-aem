cc1851d031c064190ccc10c39e22f94917ce32d2
Theofilos Papapanagiotou <theofilos@gmail.com>
2013-09-01 13:08:08 +0200

    Version 0.2.0, License and some readme changes.

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ A valid Adobe ID for authentication needs to be edited in the recipe.
 
 # Attributes #
 
+* `node['java']['jdk_version']=` - The version of Java prefered to use as the JVM of the AEM.
 * `node['aem']['adobe_id']` - Your Adobe ID used to authenticate on daycare website to download AEM.
 * `node['aem']['adobe_password']` - Your Adobe password.
+* `node['aem']['license.customer.name']` - Company name used for the license.
+* `node['aem']['license.downloadID']` - The license key.
 
 # Recipes #
 
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'chef-aem'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures chef-aem'
+maintainer       'Theofilos Papapanagiotou'
+maintainer_email 'theofilos@ieee.org'
+license          'Apache License, Version 2.0'
+description      'Installs/Configures Adobe Experience Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "apt"
 depends "java"
